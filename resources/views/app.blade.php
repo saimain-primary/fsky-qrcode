1b7c7f7e2fccee41c6f8824ff4dde282636262be
Sai Main <saimain.primary@gmail.com>
2023-12-18 15:30:11 +0630

    gallery

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
